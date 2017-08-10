ed516e27f8251ea1487077ae30c8b33ec7fc822b
Michael David Wilson <michaelwilson1991@gmail.com>
2017-08-10 17:32:43 +0800

    Update atclog.go

--- atclog.go
+++ atclog.go
@@ -2,23 +2,23 @@ package main
 
 import (
 	"fmt"
-    "io/ioutil"
-	"regexp"
 	"github.com/golang/example/stringutil"
+	"io/ioutil"
+	"regexp"
 )
 
 func main() {
 	filecontents := textreader()
 	match, _ := regexp.MatchString("atc", filecontents) // This tests whether a pattern matches a string.
-    fmt.Println(match)
+	fmt.Println(match)
 }
 
 func textreader() string {
-      f, err := ioutil.ReadFile("atc.xml") // just pass the file name
-    if err != nil {
-        fmt.Print(err)
-    }
-    filecontents := string(f) // convert content to a 'string'
-    fmt.Println(filecontents) // print the content as a 'string'
+	f, err := ioutil.ReadFile("atc.xml") // just pass the file name
+	if err != nil {
+		fmt.Print(err)
+	}
+	filecontents := string(f) // convert content to a 'string'
+	fmt.Println(filecontents) // print the content as a 'string'
 	return string(filecontents)
 }
